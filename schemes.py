87ab7b0c23a3570ff2ee29141adcef82c0f4bad2
zt-a <zt20061113@gmail.com>
2024-03-21 22:08:21 +0600

    commit

--- schemes.py
+++ schemes.py
@@ -1,6 +1,6 @@
 from typing import Optional
 
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict
 
 
 class STaskAdd(BaseModel):
@@ -10,8 +10,8 @@ class STaskAdd(BaseModel):
 
 class STask(STaskAdd):
     id: int
-    pass
 
+    model_config = ConfigDict(from_attributes=True)
 
 class STaskId(BaseModel):
     ok: bool = True
